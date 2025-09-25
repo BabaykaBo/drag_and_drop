5fb818f369fda026decaaefe93c0b508149ef83b
Oleh <zaaqq135@gmail.com>
2025-09-26 00:10:26 +0300

    added Draggable

--- src/project_item.ts
+++ src/project_item.ts
@@ -1,7 +1,9 @@
 import { Component } from "./component.js";
 import { Project } from "./project.js";
+import { Draggable } from "./drag_drop.js";
+import { autobind } from "./decorators.js";
 
-export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     constructor(hostId: string, private project: Project) {
         super('single-project', hostId, project.id);
 
@@ -10,10 +12,24 @@ export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.attach('afterbegin');
     }
 
-    protected configure() { };
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+        console.log(event);
+    }
+
+    @autobind
+    dragEndHandler(event: DragEvent): void {
+        console.log(event);
+    }
+
+    protected configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    };
+
     protected renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.numberOfPeople.toString();
+        this.element.querySelector('h3')!.textContent = `${this.project.numberOfPeople.toString()} Person${this.project.numberOfPeople === 1 ? '' : 's'} assigned`;
         this.element.querySelector('p')!.textContent = this.project.description;
     };
 }
